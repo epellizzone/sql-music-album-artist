4829624483256cea910e12653a3d6ad5194da927
epellizzone <ethan.pellizzone@gmail.com>
2023-12-11 13:46:18 +0100

    finish project

--- artists/restore.php
+++ artists/restore.php
@@ -2,7 +2,7 @@
 $artist = $_GET["id"];
 $pdo = new PDO('mysql:host=localhost;dbname=album', 'root', 'root');
 $pdo->exec('SET NAMES UTF8');
-$sql ="UPDATE artists set `IsActive` = 0 where `ArtistId`= ?";
+$sql ="UPDATE artists set `IsActive` = 1 where `ArtistId`= ?";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$artist]);
 
